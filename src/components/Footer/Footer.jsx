693e585f739378b485d992dd964b38d7f4e393e2
Bryan Lutz <bryanlutz.212@gmail.com>
2021-07-09 11:00:15 -0400

    Remove unused imports

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import './Footer.scss'
 import LinkedInLogo from '../../img/LinkedIn-White-66px-TM.png'
-import GitHubLogo from '../../img/GitHub-Mark-Light-64px.png'
-import TwitterLogo from '../../img/Twitter_Social_Icon_Circle_White.svg'
 
 const Footer = () => (
   <div className='footer'>
