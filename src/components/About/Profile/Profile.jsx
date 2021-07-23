3c18476bbe1786966a5eca59f7be6cf975108abc
Bryan Lutz <bryanlutz.212@gmail.com>
2021-07-23 12:17:55 -0400

    Adjust profiles styling

--- src/components/About/Profile/Profile.jsx
+++ src/components/About/Profile/Profile.jsx
@@ -4,14 +4,14 @@ import './Profile.scss'
 class Profile extends React.Component {
   render() {
     return (
-      <div className="row">
-        <div className="profile-card">
-          <img src={this.props.image} className='card-image' alt=''></img>
-          <div className="profile-container">
-            <p className='profile-name'>{this.props.name}</p>
-            <p className='profile-role'>{this.props.role}</p>
-            <p className='profile-text'>{this.props.text}</p>
-          </div>
+      <div className="profile-card">
+        <div className='profile-image-container'>
+          <img src={this.props.image} className='profile-image' alt=''></img>
+        </div>
+        <div className="profile-container">
+          <span className='profile-name'>{this.props.name}</span>
+          <span className='profile-role'>{this.props.role}</span>
+          <span className='profile-text'>{this.props.text}</span>
         </div>
       </div>
     );
